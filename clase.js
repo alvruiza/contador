58478a5fe34ba0e5823307c4f31ea654932550a7
alvruiza <alvaro@xocolatery.cl>
2022-06-29 16:35:22 -0400

    agregado loop para ingresar las nacionalidades al array

--- clase.js
+++ clase.js
@@ -1,35 +1,39 @@
+const natArray = []
 
-// array con los strings que queremos enumerar
-const nacionalidades = ["mexicano", "argentino", "chileno", "mexicano", "peruano", "peruano", "chileno", "mexicano", "chileno", "argentino", "chileno"];
+let numOfPersons = window.prompt("¿Cuántas personas son?");
 
-/* Función de conteo. Creamos 4 variables para cada una de las nacionalidaes.
-Aplicamos el método .filter sobre el array original 
-para encontrar las coincidencias de cada palabra, en este caso las nacionalidades.
-Cuando encontramos las coincidencias, el método .filter nos devuelve un nuevo array,
-sin modificar el original. Por ello, una vez ejecutado, 
-re definimos las variables (mex, arg, per y chi)
-con un string con la palabra que buscamos + un especio + el método .length para que cuente
-la cantidad de palabras encontradas por el método .filter en el nuevo array. Asi logramos
-obtener las veces que se repite cada palabra del array nacionalidades */
+let nationality = ""
+function input(nation) {
+    for (let i = 1; i <= nation; i++) {
+        let nat = window.prompt(`Ingrese nacionalidad ${i}.`,`Nac. Válidas: mexicano, chileno, argentino, peruano.`)
+        if (typeof nat !== "string") {
+            alert("Nacionalidad Invalida")
+            i = i -1            
+        }else{
+            console.log(`${nat} fue agregado al arreglo`)
+            nationality = nat 
+            natArray.push(nat)           
+        }
+    }
+}
 
-function cuenta() {
+input(numOfPersons)
 
-let mex = nacionalidades.filter (element => element === "mexicano")
+function count() {
+
+let mex = natArray.filter (element => element === "mexicano")
 mex = "Mexicanos: " + " " + mex.length
-let arg = nacionalidades.filter (element => element === "argentino")
+let arg = natArray.filter (element => element === "argentino")
 arg = "Argentinos: " + " " + arg.length
-let per = nacionalidades.filter (element => element === "peruano")
+let per = natArray.filter (element => element === "peruano")
 per = "Peruanos: " + " " + per.length
-let chi = nacionalidades.filter (element => element === "chileno")
+let chi = natArray.filter (element => element === "chileno")
 chi = "Chilenos: " + " " + chi.length
 
-/*  Finalmente le pedimos a la funcion que 
- en loguee en la consola las 4 variables para ver los resultados */
-
-console.log(mex, arg, per,chi)
+console.log(chi,mex,per,arg)
+alert(`${chi}, ${mex}, ${per}, ${arg}`)
 
 }
 
-// llamamos a la función creada para ver los resultados en la consola.
+count() 
 
-cuenta()
